0e6f1797ae3ff9ee853cbeca3bc386052b765dd2
ruxiuhsiao <40812662+ruxiuhsiao@users.noreply.github.com>
2020-06-23 22:15:37 +0800

    Add files via upload

--- final/js/poker.js
+++ final/js/poker.js
@@ -1,4 +1,5 @@
 /////player1
+var poker1_g = []
 // 產生撲克牌在網頁上
 var newPoker = (r, i) => {
     // 產生 img 的 jQuery 物件在變數 $img
@@ -11,7 +12,6 @@ var newPoker = (r, i) => {
     // 當img被按到的時候
     $img.on('click', (event) => {
         let $poker = $(event.target)
-
         // 判斷目前牌是蓋著還是翻
         if ($poker.attr('data-back') == 'true') {
             // 目前牌是蓋著，要執行翻牌動作
@@ -33,16 +33,6 @@ var newPoker = (r, i) => {
 
     // 將 $div 插入到網頁 id=data 的html element 裡面
     $('#data1').append($div)
-
-    /*
-    let $button = $('<button>').attr('id', 'bt' + i)
-    $button.attr('type', 'button').attr('class', 'btn-primary').attr('class', 'btn')
-    //< button id = "close1" type = "button" class="btn btn-primary" > 蓋牌</button >
-
-    $div = $('<div>').addClass('col').addClass('poker2').attr('id', 'd2')
-    $div.append($button)
-    $('#data11').append($button)
-    */
 }
 
 $(() => {
@@ -66,10 +56,17 @@ $(() => {
             poker[0] = temp
         }
 
+        var poker1 = []
+        for (let i = 0; i < 5; i++) {
+            poker1[i] = poker[i]
+        }
         // 發牌
         for (let i = 0; i < 5; i++) {
             newPoker(poker[i], i)
         }
+
+        poker1_g = poker1
+        return poker1_g
     })
 })
 
@@ -77,20 +74,14 @@ $(() => {
 //開牌
 $(() => {
     $('#open1').on('click', () => {
-        // 洗空桌面
-        //$('#data1').empty();
 
         for (let i = 0; i < 5; i++) {
             //var temp = '#' + i
             // 產生 div 的 jQuery 物件在變數 $div
             var temp = $('#' + i).attr("data-poker")
-            $('#' + i).attr('src', './poker/pic' + temp + '.png')
+            $('#' + i).attr('src', './poker/pic' + (poker1_g[i] + 1) + '.png')
+            //$('#' + i).attr('src', './poker/pic' + temp + '.png')
             $('#' + i).attr('data-back', false)
-            //$div = $('<div>').addClass('col').addClass('poker')
-            // 將 $img 插入到 $div 內
-            //$div.append($('#' + i))
-            // 將 $div 插入到網頁 id=data 的html element 裡面
-            //$('#data1').append($div)
         }
     })
 })
@@ -118,7 +109,7 @@ $(() => {
 
 /////player2
 
-//var poker2_g = new2();
+var poker2_g = []
 
 // 產生撲克牌在網頁上
 var newPoker2 = (r, i) => {
@@ -179,29 +170,29 @@ $(() => {
             poker2[0] = temp
         }
 
+        var poker2_2 = []
+        for (let i = 0; i < 5; i++) {
+            poker2_2[i] = poker2[i]
+        }
         // 發牌
         for (let i = 0; i < 5; i++) {
             newPoker2(poker2[i], i + 5)
         }
+        poker2_g = poker2_2
+        return poker2_g
     })
 })
 
 //開牌
 $(() => {
     $('#open2').on('click', () => {
-        // 洗空桌面
-        //$('#data1').empty();
-
         for (let i = 5; i < 10; i++) {
             //var temp = '#' + i
             // 產生 div 的 jQuery 物件在變數 $div
-            $('#' + i).attr('src', './poker/pic' + $('#' + i).attr("data-poker") + '.png')
+            var temp = $('#' + i).attr("data-poker")
+            $('#' + i).attr('src', './poker/pic' + (poker2_g[i - 5] + 1) + '.png')
+            //$('#' + i).attr('src', './poker/pic' + temp + '.png')
             $('#' + i).attr('data-back', false)
-            //$div = $('<div>').addClass('col').addClass('poker')
-            // 將 $img 插入到 $div 內
-            //$div.append($('#' + i))
-            // 將 $div 插入到網頁 id=data 的html element 裡面
-            //$('#data1').append($div)
         }
     })
 })
@@ -217,12 +208,7 @@ $(() => {
             // 產生 div 的 jQuery 物件在變數 $div
             $('#' + i).attr('src', './poker/back.png')
             $('#' + i).attr('data-back', true)
-            //$('#' + i).attr('src', './poker/pic' + $('#' + i).attr("data-poker") + '.png')
             $div = $('<div>').addClass('col').addClass('poker')
-            // 將 $img 插入到 $div 內
-            //$div.append($('#' + i))
-            // 將 $div 插入到網頁 id=data 的html element 裡面
-            //$('#data1').append($div)
         }
     })
 })
