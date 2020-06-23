85e920d17e4f8f2f01f15383ce01345290abadd2
ruxiuhsiao <40812662+ruxiuhsiao@users.noreply.github.com>
2020-06-23 17:56:03 +0800

    Add files via upload

--- final/js/poker.js
+++ final/js/poker.js
@@ -1,60 +1,4 @@
-$(() => {
-    $('#main').on('click', () => {
-        // 隨機產生一個1到52的數字
-        let r = RandomInt(1, 52)
-        // 產生 img 的 jQuery 物件在變數 $img
-        $img = $('<img>').attr('src', './poker/pic' + r + '.png')
-        // 產生 div 的 jQuery 物件在變數 $div
-        $div = $('<div>').addClass('col').addClass('poker')
-        // 將 $img 插入到 $div 內
-        $div.append($img)
-        // 將 $div 插入到網頁 id=data 的html element 裡面
-        $('#data').append($div)
-    })
-})
-
-var newPoker = (r) => {
-    // 產生 img 的 jQuery 物件在變數 $img
-    $img = $('<img>').attr('src', './poker/pic' + (r + 1) + '.png')
-
-    // 產生 div 的 jQuery 物件在變數 $div
-    $div = $('<div>').addClass('col').addClass('poker')
-
-    // 將 $img 插入到 $div 內
-    $div.append($img)
-
-    // 將 $div 插入到網頁 id=data 的html element 裡面
-    $('#data').append($div)
-}
-
-$(() => {
-    $('#main').on('click', () => {
-
-        // 洗空桌面
-        $('#data').empty();
-
-        // 產生新撲克牌
-        var poker = []
-        for (let i = 0; i < 52; i++) {
-            poker.push(i)
-        }
-
-        // 洗牌
-        for (let i = 0; i < 100; i++) {
-            // 隨機抽第 r 張，和第一張交換
-            let r = RandomInt(0, 51)
-            let temp = poker[r]
-            poker[r] = poker[0]
-            poker[0] = temp
-        }
-
-        // 發牌
-        for (let i = 0; i < 5; i++) {
-            newPoker(poker[i])
-        }
-    })
-})
-
+/////player1
 // 產生撲克牌在網頁上
 var newPoker = (r) => {
     // 產生 img 的 jQuery 物件在變數 $img
@@ -90,6 +34,7 @@ var newPoker = (r) => {
     $('#data').append($div)
 }
 
+//create 5 poker
 $(() => {
     $('#main').on('click', () => {
 
@@ -114,6 +59,72 @@ $(() => {
         // 發牌
         for (let i = 0; i < 5; i++) {
             newPoker(poker[i])
+        }
+    })
+})
+
+/////player2
+///player1
+// 產生撲克牌在網頁上
+var newPoker2 = (r) => {
+    // 產生 img 的 jQuery 物件在變數 $img
+    let $img = $('<img>').attr('src', './poker/back.png')
+    $img.attr('data-poker', r)
+    $img.attr('data-back', true)
+
+    // 當img被按到的時候
+    $img.on('click', (event) => {
+        let $poker = $(event.target)
+
+        // 判斷目前牌是蓋著還是翻開
+        if ($poker.attr('data-back') == 'true') {
+            // 目前牌是蓋著，要執行翻牌動作
+            let r = $poker.attr('data-poker')
+            r = Number(r)
+            $poker.attr('src', './poker/pic' + (r + 1) + '.png')
+            $poker.attr('data-back', false)
+        } else {
+            // 目前牌是打開的，要執行蓋牌的動作
+            $poker.attr('src', './poker/back.png')
+            $poker.attr('data-back', true)
+        }
+    })
+
+    // 產生 div 的 jQuery 物件在變數 $div
+    $div = $('<div>').addClass('col').addClass('poker')
+
+    // 將 $img 插入到 $div 內
+    $div.append($img)
+
+    // 將 $div 插入到網頁 id=data 的html element 裡面
+    $('#data2').append($div)
+}
+
+//create 5 poker
+$(() => {
+    $('#main2').on('click', () => {
+
+        // 洗空桌面
+        $('#data2').empty();
+
+        // 產生新撲克牌
+        var poker = []
+        for (let i = 0; i < 52; i++) {
+            poker.push(i)
+        }
+
+        // 洗牌
+        for (let i = 0; i < 100; i++) {
+            // 隨機抽第 r 張，和第一張交換
+            let r = RandomInt(0, 51)
+            let temp = poker[r]
+            poker[r] = poker[0]
+            poker[0] = temp
+        }
+
+        // 發牌
+        for (let i = 0; i < 5; i++) {
+            newPoker2(poker[i])
         }
     })
 })
